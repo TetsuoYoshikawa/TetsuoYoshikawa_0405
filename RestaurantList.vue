d88bb6c0c476c5e405ef1e435db2d83a6c53860a
TetsuoYoshikawa <77278965+TetsuoYoshikawa@users.noreply.github.com>
2021-08-02 17:28:25 +0900

    Update RestaurantList.vue

--- RestaurantList.vue
+++ RestaurantList.vue
@@ -1,26 +1,45 @@
+
 <template>
   <div>
-    <HeaderAuth />
+    <div class="header">
+      <img class="header-img" src="../assets/store.png" />
+      <h2 class="header-title">RESE</h2>
+      <div class="right flex">
+        <select v-model="searchData.prefecture">
+          <option value="">All Prefecutes</option>
+          <option v-for="prefecture in prefectures" :key="prefecture.name">{{prefecture.name}}</option>
+        </select>
+        <select v-model="searchData.genre">
+          <option value="">All Genre</option>
+          <option v-for="genre in genres" :key="genre.name">{{genre.name}}</option>
+        </select>
+        <input type="text" placeholder="Restaurant Name" v-model="searchData.restaurant">
+        <button class="search" type="submit" @click="storeSearch">Search</button>
+      </div>
+      <div class="mypage">
+        <button type="submit" @click="$router.push({path: '/mypage'}, () => {})">マイページ</button>
+      </div>
+    </div>
     <div class="restaurant-list contents">
       <div class="item">
-        <div class="restaurant-card flex" v-for="(restaurant,index) in restaurants" :key="index">
-          <img :src=restaurant.url class="restaurant-pic">
+        <div class="restaurant-card flex" v-for="restaurant in restaurants" :key="restaurant.id">
+          <img :src=restaurant.image_url class="restaurant-pic">
           <div class="restaurant-detail">
             <div class="restaurant-name">
               <p>{{restaurant.name}}</p>
             </div>
             <div class="tag">
-              <p>#{{restaurant.prefecture}} #{{restaurant.genre}}</p>
+              <p>#{{restaurant.prefecture.name}} #{{restaurant.genre.name}}</p>
             </div>
-            <div>
+            <div class="button">
               <button @click="
-              $router.push({
-                path:'/detail/',
+                $router.push({
+                path:'/detail/' + restaurant.id,
                 name:'Detail',
-                params:{id:restauranr.id}})">詳しく見る</button>
-              <vue-star animate="animated rubberBand" color="#F05654">
-                <a slot="icon" class="fa fa-heart" @click="fav(restaurant)"></a>
-              </vue-star>
+                params:{id:restaurant.id}})">詳しく見る</button>
+              <v-icon name="heart" scale="2" class="heart" @click="favoriteDelete(restaurant)">
+              </v-icon>
+              <img src="../assets/heart_red.png" @click="favoritePost(restaurant)" style="height:30px;width:30px;" />
             </div>
           </div>
         </div>
@@ -30,59 +49,219 @@
 </template>
 
 <script>
-import VueStar from 'vue-star';
+import 'vue-awesome/icons';
+import Icon from 'vue-awesome/components/Icon';
 import HeaderAuth from '../components/HeaderAuth.vue';
+import axios from "axios";
 export default{
   props:['id'],
   data(){
     return{
-      restaurants: []
+      restaurants: [],
+      prefectures:[],
+      genres:[],
+      favorites:[],
+      restaurants: [],
+      prefectures:[],
+      genres:[],
+      searchData:{
+        prefecture:"",
+        genre:"",
+        restaurant:"",
+      },
+      keyword:""
     }
   },
-  mounted: function(){
-    this.$refs.ThumbsUp.$data.active = true;
-    console.log(this.$refs.ThumbsUp.$data);
+  computed:{
+    storeSearch(){
+      return this.restaurants.filter(restaurant => {
+        return restaurant.prefecture.name.includes(this.searchData.prefecture) ||
+        restaurant.grnre.name.includes(this.searchData.genre) ||
+        restaurant.prefecture.name.includes(this.searchData.restaurant)
+      })
+    }
   },
   methods:{
+    storeSearch(){
+      for(let restaurant of this.restaurants){
+        let existerPrefectureId;
+        if(restaurant.prefecture.name.includes(this.searchData.prefecture)){
+          existerPrefectureId = true;
+        }else{
+          existerPrefectureId = false;
+        }
+
+        let existerGenreId;
+        if(restaurant.genre.name.includes(this.searchData.genre)){
+          existerGenreId = true;
+        }else{
+          existerGenreId = false;
+        }
+
+        let existerRestaurantId;
+        if(restaurant.name.includes(this.searchData.searchData.restaurant)){
+          existerRestaurantId = true;
+        }else{
+          existerRestaurantId = false;
+        }
+      }
+    },
     async getRestaurant(){
       await axios
-        .get("https//127.0.0.1:8000/api/v1/restaurants")
+        .get("http://127.0.0.1:8000/api/restaurants")
         .then((response => {
           this.restaurants = response.data.data.restaurant;
         }))
         .catch(error => {
           console.log(error)});
     },
-    fav(restaurant) {
-      console.log(this.$refs.ThumbsUp.$data);
-      axios
-        .post("http://127.0.0.1:8000/api/v1/favorite",{
-          user_id:this.$store.state.user.id,
-          restaurant_id:store.restaurant.id
-        })
+    async getFavorite(){
+      await axios
+        .get('http://127.0.0.1:8000/api/favorites')
         .then((response) => {
-          console.log(response);
-          this.$router.go({
-            path:this.$router.currentRoute.path,
-            force:true,
-          });
+          this.favorites = response.data.data;
+          if(this.favorites === 0){
+            this.notFavorite = true;
+          }else{
+            this.notFavorite = false;
+          }
+        })
+        .catch((error) => {
+          console.log(error)
         });
     },
+    async favoritePost(restaurant){
+      await axios
+      .post('http://127.0.0.1:8000/api/favorites',{
+        user_id:this.$store.state.user_id,
+        restaurant_id:restaurant.id,
+      })
+      .then((response) => {
+        console.log(response);
+        alert('いいねしました');
+      });
+    },
+    async favoriteDelete(restaurant){
+      await axios
+      .delete('http://127.0.0.1:8000/api/favorites',{
+        data:{
+          user_id:this.$store.state.user_id,
+          restaurant_id:restaurant.id
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        alert('いいねを削除しました')
+      })
+    },
+    async getPrefecture(){
+      await axios
+        .get("http://127.0.0.1:8000/api/prefectures")
+        .then((response) => {
+          this.prefectures = response.data.data;
+        })
+    },
+    async getGenre(){
+      await axios
+        .get("http://127.0.0.1:8000/api/genres")
+        .then((response) => {
+          this.genres = response.data.data;
+        })
+    },
+    async storeSearch() {
+      await axios
+        .get(
+          "http://127.0.0.1:8000/api/searchRestaurants/" +
+            this.$store.state.user.id,
+          {
+            params: {
+              name: this.searchrestaurant,
+              prefecture_id: this.searchPrefecture,
+              genre_id: this.searchGenre,
+            },
+          }
+        )
+        .then((response) => {
+          this.stores = response.data.data;
+          this.searchResult = false;
+          this.loading = false;
+        })
+    }
   },
   components:{
     HeaderAuth,
-    'VueStar':VueStar,
+    'v-icon':Icon,
   },
   created(){
-    getRestaurant();
+    this.getRestaurant();
+    this.getFavorite();    
+    this.getPrefecture();
+    this.getGenre();
   }
 };
 </script>
 
 <style scoped>
 /*////////////////
+    ヘッダー
+////////////////*/
+.header{
+  display:flex;
+  height:70px;
+  align-items: center;
+  background-color:#ff7300;
+}
+.header-img{
+  width:20px;
+  height: 20px;
+  margin:0 20px;
+}
+.header-title{
+  color:white;
+  font-size:25px;
+}
+.right{
+  align-items:center;
+  margin: 0 auto;
+}
+.right p {
+  margin-right:20px;
+  cursor: pointer;
+}
+.logo{
+  width:150px;
+  cursor: pointer;
+}
+select{
+  padding:15px;
+  border:none;
+  font-size: 16px;
+}
+input{
+  padding:17px;
+  border:none;
+}
+.search{
+  background-color:white;
+  padding:17px;
+  border:none;
+  margin-top:1px;
+}
+.mypage{
+  margin:0 20px;
+  border-radius: 10px;
+}
+.mypage button{
+  border-radius: 30px;
+  background-color:white;
+}
+/*////////////////
     店舗情報
 ////////////////*/
+.button{
+  display: flex;
+  flex-wrap: wrap;
+}
 .item{
   width:90%;padding-left:20px;
   margin:0 auto;
@@ -108,6 +287,10 @@ export default{
   font-size:20px;
   padding-bottom: 15px;
 }
+.heart{
+  padding-left:80px;
+  color:#F05654;
+}
 button{
   padding:10px 20px;
   margin-left: 20px;
@@ -115,11 +298,5 @@ button{
   border:none;
   border-radius: 10px;
 }
-.VueStar {
-      position: relative;
-    }
-    .VueStar__icon .fa {
-      font-size: 3em;
-      cursor: pointer;
-    }
+
 </style>
