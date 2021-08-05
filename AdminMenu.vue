8546b8aa31cab8a1835dfda6a8107bd2f125d306
TetsuoYoshikawa <77278965+TetsuoYoshikawa@users.noreply.github.com>
2021-08-05 17:25:33 +0900

    Add files via upload

---
+++ AdminMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="side_menu">
+      <Header />
+      <div class="side_menu">
+        <div class="flex icon-filed" @click="restaurantAdd">
+          <img src="../assets/store.png" style="height:30px;width:30px">
+          <p>店舗情報編集</p>
+        </div>
+        <div class="flex icon-field" @click="restaurantList">
+          <img src="../assets/store.png" style="height:30px;width:30px">
+          <p>予約内容確認</p>
+        </div>
+        <div class="flex icon-field" @click="logout">
+          <img src="../assets/logout.png">
+          <p>ログアウト</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.flex {
+  display: flex;
+}
+img {
+  margin: 10px 20px 10px;
+  height:30px;
+  width:30px;
+}
+
+</style>
---
+++ MyData.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <div class="restaurant-card flex"  v-for="restaurant in restaurants" :key="restaurant.id">
+      <div class="owner-name">
+        <p>Your Name:{{$store.state.user_name}}</p>
+        <p>Your E-mail:{{$store.state.user_email}}</p>
+      <div class="restaurant-name" >
+        <img class="img" src="../assets/Back.png" @click="$router.push('/')">
+        <h2 class="restaurant-title">No.{{restaurant.id}}:{{restaurant.name}}</h2>
+      </div>
+      <img :src=restaurant.image_url class="restaurant-pic">
+      <div class="restaurant-detail">
+        <div class="tag" style="padding:20px;font-size:20px"> 
+          <p>Prefecture:{{restaurant.prefecture.name}} </p>
+          <p>Genre:{{restaurant.genre.name}}</p>
+        </div>
+        <div class="description" style="padding-left:20px;font-size:16px">
+          <p>{{restaurant.description}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
